90cd7a9e1ac53aade803674078768b95849355c5
Chaitanya-Kalme <chaitanyakalme2005@gmail.com>
2025-09-24 23:54:02 +0530

    import change error in layour.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 import type { Metadata } from "next";
-import { GeistSans } from '@vercel/geist/font/sans';
-import { GeistMono } from '@vercel/geist/font/mono';
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/providers";
 import { Toaster } from "@/components/ui/sonner";
 
-const geistSans = GeistSans;
-const geistMono = GeistMono;
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
